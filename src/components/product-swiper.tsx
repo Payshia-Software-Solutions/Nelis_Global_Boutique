e73766fefd8ca998cdcb7d4f613c64b324845728
Thilina Ruwan <thilinaruwan112@gmail.com>
2025-09-02 05:43:59 +0000

    reduce the gap between products and add some left padding as well

--- src/components/product-swiper.tsx
+++ src/components/product-swiper.tsx
@@ -33,11 +33,11 @@ export function ProductSwiper({ products }: ProductSwiperProps) {
           }}
           className="w-full"
         >
-          <CarouselContent>
+          <CarouselContent className="-ml-2">
             {products.map((product) => (
               <CarouselItem 
                 key={product.id} 
-                className="basis-[calc(100%/1.5)] sm:basis-[calc(100%/2.5)] md:basis-[calc(100%/3.5)] lg:basis-[calc(100%/4.5)]"
+                className="basis-[calc(100%/1.5)] sm:basis-[calc(100%/2.5)] md:basis-[calc(100%/3.5)] lg:basis-[calc(100%/4.5)] pl-2"
               >
                 <div className="p-1">
                   <ProductCard product={product} />
