f7686b7eb44f55876d1057c30daed1290438561b
Yomal theekshana <yomaltheekshana66@gmail.com>
2025-09-13 18:06:38 +0000

    in mobile mood show one full card and half part of other card  (_for ele

--- src/components/product-swiper.tsx
+++ src/components/product-swiper.tsx
@@ -37,7 +37,7 @@ export function ProductSwiper({ products }: ProductSwiperProps) {
             {products.map((product) => (
               <CarouselItem 
                 key={product.id} 
-                className="basis-full sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+                className="basis-4/5 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
               >
                 <div className="p-1">
                   <ProductCard product={product} />
